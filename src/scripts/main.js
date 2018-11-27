dd0a86e46aab211645e4eb83e9c4205ab787420d
brunoannunciato <brunobannunciato@gmail.com>
2018-11-26 23:00:16 -0200

    add froala

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,12 +11,10 @@ $(document).ready(function(){
 	.setClassToggle('#header', 'fixed') // add class to project01
     .addTo(controller);
 
-    console.log($('.obj-transition'))
-    
     $('.obj-transition').each(function(obj) {
         new ScrollMagic.Scene({
             triggerElement: this,
-            triggerHook: 0.8
+            triggerHook: 0.7
         })
         .setClassToggle(this, 'show')
         .addTo(controller);
@@ -28,5 +26,26 @@ $(document).ready(function(){
         centerMode: true,
         infinite: false
     });
+
+    $('.tab-header').each(function(indice, obj) {
+        $(obj).click(function() {
+            $(this).parent().toggleClass('show');
+        })
+    })
+
+    $('.application-slide').each(function(indice, obj) {
+        $(obj).slick({
+            arrows: true
+        });
+    })
+
+    $('#froala-editor').froalaEditor({
+        toolbarInline: true,
+        charCounterCount: false,
+        placeholderText: 'Digite o texto',
+        toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'color', '-', 'paragraphFormat', 'align']
+    });
+    
+    
 
 });
